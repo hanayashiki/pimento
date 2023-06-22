573e86f708695a31a16b1f8f5c7e705dfb8158a4
hanayashiki <cwang592@usc.edu>
2023-06-23 01:19:48 +0900

    feat: dont fetch data not immediately needed

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -9,22 +9,31 @@ import {
 import getQueryClient from "../getQueryClient";
 import Hydrate from "@/lib/client/Hydrate";
 
-export default async function Page() {
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: { type?: string };
+}) {
   const queryClient = getQueryClient();
 
+  const { type = "TextPassword" } = searchParams;
+
   await Promise.all([
-    queryClient.fetchQuery({
-      queryKey: ["listTextPassword", { search: "", orders: [] }],
-      queryFn: () => listTextPassword({ search: "", orders: [] }),
-    }),
-    queryClient.fetchQuery({
-      queryKey: ["listAccountPassword", { search: "", orders: [] }],
-      queryFn: () => listAccountPassword({ search: "", orders: [] }),
-    }),
-    queryClient.fetchQuery({
-      queryKey: ["listPaymentCard", { search: "", orders: [] }],
-      queryFn: () => listPaymentCard({ search: "", orders: [] }),
-    }),
+    type === "TextPassword" &&
+      queryClient.fetchQuery({
+        queryKey: ["listTextPassword", { search: "", orders: [] }],
+        queryFn: () => listTextPassword({ search: "", orders: [] }),
+      }),
+    type === "AccountPassword" &&
+      queryClient.fetchQuery({
+        queryKey: ["listAccountPassword", { search: "", orders: [] }],
+        queryFn: () => listAccountPassword({ search: "", orders: [] }),
+      }),
+    type === "PaymentCard" &&
+      queryClient.fetchQuery({
+        queryKey: ["listPaymentCard", { search: "", orders: [] }],
+        queryFn: () => listPaymentCard({ search: "", orders: [] }),
+      }),
   ]);
 
   const dehydratedState = dehydrate(queryClient);
