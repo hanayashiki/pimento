544d848e3acbe18716878517ce46f22aceb2ec5e
hanayashiki <wangchenyu2017@gmail.com>
2024-06-01 23:24:55 +0900

    fix: avoid flash closing on chrome

--- chrome-extension/popup.js
+++
@@ -1,9 +0,0 @@
-
-chrome.webRequest.onBeforeSendHeaders.addListener(
-    function (details) {
-        console.log(details);
-        return { requestHeaders: details.requestHeaders };
-    },
-    { urls: ["<all_urls>"] },
-    ["blocking", "requestHeaders"]
-);
